381b4e8935fb6bd662c16c2614e9d8f6f9366c28
Kaleb Ercanbrack <kaleb.ercanbrack@hey.com>
2023-08-20 16:36:37 -0600

    redirect with messages

--- src/index.ts
+++ src/index.ts
@@ -13,12 +13,30 @@ const router = createRouter().post(
   lazyEventHandler(() =>
     eventHandler(async (event) => {
       const request = toWebRequest(event);
-      await event.context.ROSTER_BUCKET.put("reports.tar.gz", request.body);
-      return sendRedirect(event, "http://localhost:8000/");
+      try {
+        await event.context.ROSTER_BUCKET.put("reports.tar.gz", request.body);
+      } catch (err) {
+        return sendRedirect(
+          event,
+          `http://localhost:8000/?message=${encodeURIComponent(
+            "error saving file"
+          )}`
+        );
+      }
+      //don't await so we can redirect sooner
+      fetch(
+        "https://api.cloudflare.com/client/v4/pages/webhooks/deploy_hooks/eca10840-bf7e-4605-8c8b-026cf8238618",
+        {
+          method: "POST",
+        }
+      );
+      sendRedirect(
+        event,
+        `http://localhost:8000/?message=${encodeURIComponent("done!")}`
+      );
     })
   )
-);
-
+)
 
 const app = createApp();
 app.use(router);
