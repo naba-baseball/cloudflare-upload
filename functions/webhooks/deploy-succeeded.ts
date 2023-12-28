16897eef3e86cb3ac4b762f3cb80e0e52e4ae854
Kaleb Ercanbrack <44715857+Twitch0125@users.noreply.github.com>
2023-12-27 21:57:30 -0700

    Update deploy-succeeded.ts

--- functions/webhooks/deploy-succeeded.ts
+++ functions/webhooks/deploy-succeeded.ts
@@ -8,7 +8,7 @@ export const onRequestPost: PagesFunction<Env> = async (context) => {
   await fetch(context.env.DISCORD_WEBHOOK, {
     method: "POST",
     body: JSON.stringify({
-      content: "<@&1147023121682006106> Website deployed!",
+      content: "<@&1147023121682006106> Website updated: https://nabaleague.com",
     }),
     headers: {
       "Content-Type": "application/json",
